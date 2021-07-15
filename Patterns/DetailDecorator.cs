41ba5694799802e11bd821a22138c0498c484389
AlSurick <surickal@yandex.ru>
2021-07-15 12:52:55 +0300

    Add detail structure with decorators

---
+++ Patterns/DetailDecorator.cs
@@ -0,0 +1,13 @@
+﻿namespace Patterns
+{
+    public abstract class DetailDecorator: ICabinDetail
+    {
+        protected DetailDecorator(string name, ICabinDetail detail) : 
+            base(name)
+        {
+            this.detail = detail;
+        }
+
+        protected ICabinDetail detail;
+    }
+}
---
+++ Patterns/ICabinDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace Patterns
+{
+    public abstract class ICabinDetail
+    {
+        public ICabinDetail(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+        public abstract int GetCost();
+    }
+}
